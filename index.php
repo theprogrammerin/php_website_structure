0bd856f1815f74341580b7801574c14a12c6af57
Ashutosh Agrawal <Ashutosh_Agrawal@live.in>
2013-03-03 17:38:26 +0530

    Adding default route for home page

--- index.php
+++ index.php
@@ -3,39 +3,43 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>SRMMUN</title>
-		
+
 		<link rel="stylesheet" type="text/css" href="_styles/style.css" />
-		
+
 		<script src="_scripts/jquery.js" />
 		<script src="_scripts/loader.js" />
 	</head>
 	<body>
-		
+
 		<!-- Header Start -->
-		
+
 		<?php include_once 'header.php'; ?>
-		
+
 		<!-- Header Ends -->
-		
-		
+
+
 		<!-- Page Starts -->
-		
-		<?php 
-		
-		$url = $_GET['page'];
+
+		<?php
+
+		$url = "home";
+		if($_GET['page'])
+		{
+			$url = $_GET['page'];
+		}
 		$url = $url.".php";
-		
-		include_once $url 
-		
+
+		include_once $url
+
 		?>
-		
+
 		<!-- Page Ends -->
-		
-		
+
+
 		<!-- Footer Starts  -->
-		
+
 		<?php include_once 'footer.php'; ?>
-		
+
 		<!-- Footer Ends -->
 	</body>
 </html>
